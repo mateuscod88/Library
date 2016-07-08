19aeb6bf7b9ccfd2c5f49216e4b13f4d343c5fcd
mateusz.malinowski <mateusz.malinowski@WS-2P43L22>
2016-07-08 15:21:01 +0200

    grid Grid / Basic usage

--- LibraryMVC/Controllers/BookController.cs
+++ LibraryMVC/Controllers/BookController.cs
@@ -28,38 +28,7 @@ namespace LibraryMVC.Controllers
             return Json(_bookService.RetriveAll().ToDataSourceResult(request));
         }
 
-        [AcceptVerbs(HttpVerbs.Post)]
-        public ActionResult EditingPopup_Create([DataSourceRequest] DataSourceRequest request, AddBookViewModel addbookViewModel)
-        {
-            if (addbookViewModel != null && ModelState.IsValid)
-            {
-                _bookService.AddNewBook(addbookViewModel);
-            }
-
-            return Json(new[] { addbookViewModel }.ToDataSourceResult(request, ModelState));
-        }
-
-        [AcceptVerbs(HttpVerbs.Post)]
-        public ActionResult EditingPopup_Update([DataSourceRequest] DataSourceRequest request, BookViewModel product)
-        {
-            if (product != null && ModelState.IsValid)
-            {
-                productService.Update(product);
-            }
-
-            return Json(new[] { product }.ToDataSourceResult(request, ModelState));
-        }
-
-        [AcceptVerbs(HttpVerbs.Post)]
-        public ActionResult EditingPopup_Destroy([DataSourceRequest] DataSourceRequest request, ProductViewModel product)
-        {
-            if (product != null)
-            {
-                productService.Destroy(product);
-            }
-
-            return Json(new[] { product }.ToDataSourceResult(request, ModelState));
-        }
+        
 
     }
 }
